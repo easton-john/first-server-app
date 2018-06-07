febc5595ecd391310ed9100367931c5829b2b618
Easton Gorishek <ieaston26@gmail.com>
2018-06-07 16:49:11 -0700

    CRUD is in working status

--- server/server.js
+++ server/server.js
@@ -18,9 +18,11 @@ client.connect();
 app.get('/api/cocktails', (req, res) => {
 
   client.query(`
-  SELECT 
-  
-  FROM cocktails;
+  SELECT cocktails.id, name, alcohols.id as "alcoholID", alcohols.alcohol, ingredients,
+  garnish, tried, image
+  FROM cocktails
+  JOIN alcohols
+    ON cocktails.alcohol_id = alcohols.id;
   `).then(result => {
     res.send(result.rows);
   });
@@ -31,11 +33,11 @@ app.post('/api/cocktails', (req, res) => {
   const body = req.body;
 
   client.query(`
-    INSERT INTO cocktails (name, alcohol, ingredients, served, garnish, tried, image)
+    INSERT INTO cocktails (name, alcohol_id, ingredients, served, garnish, tried, image)
     VALUES ($1, $2, $3, $4, $5, $6, $7)
     RETURNING *;
   `,
-  [body.name, body.alcohol, body.ingredients, body.served, body.garnish, body.tried, body.image]
+  [body.name, body.alcoholID, body.ingredients, body.served, body.garnish, body.tried, body.image]
   ).then(result => {
     // send back object
     res.send(result.rows[0]);
@@ -43,14 +45,35 @@ app.post('/api/cocktails', (req, res) => {
 
 });
 
+app.put('/api/cocktails/:id', (req, res) => {
+  const body = req.body;
+
+  client.query(`
+    UPDATE cocktails
+    SET
+      name = $1,
+      alcohol_id=$2,
+      ingredients = $3,
+      served = $4,
+      garnish = $5,
+      tried = $6,
+      image = $7
+    WHERE id = $8
+    RETURNING *;
+  `,
+  [body.name, body.alcoholID, body.ingredients, body.served, body.garnish, body.tried, body.image, req.params.id]
+  ).then(result => {
+    res.send(result.rows[0]);
+  });
+});
+
 app.delete('/api/cocktails/:id', (req, res) => {
-  const params = req.params.id;
 
   client.query(`
     DELETE FROM cocktails 
       WHERE id=$1;
     `,
-  [params]).then(() => {
+  [req.params.id]).then(() => {
   
     res.send({ removed: true });
   });
